42a46747e29ed7fe226ecb7689a3f3a0f79fcab5
hyunkyu <azdlgusrb@naver.com>
2025-01-25 17:26:47 +0900

    :wrench: "security config 추가"
    security 세팅 추가 작업
    
    Resolves: FLIZ-48
    Related to: FLIZ-46

---
+++ src/main/java/spring/fitlinkbe/support/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package spring.fitlinkbe.support.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+            )
+                .oauth2Login(oauth2 -> {});
+        return http.build();
+    }
+}
