e7216739af626567e7753857b6fabaa18530ab2b
gt.park <smileboy0014@gmail.com>
2025-02-04 00:04:53 +0900

    :recycle: "PersonalDetail 엔터티 관련 리팩토링 진행"

--- src/main/java/spring/fitlinkbe/domain/common/PersonalDetailRepository.java
+++ src/main/java/spring/fitlinkbe/domain/common/PersonalDetailRepository.java
@@ -2,8 +2,15 @@ package spring.fitlinkbe.domain.common;
 
 import spring.fitlinkbe.domain.common.model.PersonalDetail;
 
+import java.util.Optional;
+
 public interface PersonalDetailRepository {
+
+    Optional<PersonalDetail> savePersonalDetail(PersonalDetail personalDetail);
+
     PersonalDetail saveIfNotExist(PersonalDetail personalDetail);
 
     PersonalDetail getById(Long personalDetailId);
+
+    Optional<PersonalDetail> getTrainerDetail(Long trainerId);
 }
--- src/main/java/spring/fitlinkbe/infra/common/personaldetail/PersonalDetailRepositoryImpl.java
+++ src/main/java/spring/fitlinkbe/infra/common/personaldetail/PersonalDetailRepositoryImpl.java
@@ -1,4 +1,4 @@
-package spring.fitlinkbe.infra.common;
+package spring.fitlinkbe.infra.common.personaldetail;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -6,7 +6,6 @@ import spring.fitlinkbe.domain.common.PersonalDetailRepository;
 import spring.fitlinkbe.domain.common.exception.CustomException;
 import spring.fitlinkbe.domain.common.exception.ErrorCode;
 import spring.fitlinkbe.domain.common.model.PersonalDetail;
-import spring.fitlinkbe.infra.common.model.PersonalDetailEntity;
 
 import java.util.Optional;
 
@@ -15,6 +14,14 @@ import java.util.Optional;
 public class PersonalDetailRepositoryImpl implements PersonalDetailRepository {
 
     private final PersonalDetailJpaRepository personalDetailJpaRepository;
+
+    @Override
+    public Optional<PersonalDetail> savePersonalDetail(PersonalDetail personalDetail) {
+        PersonalDetailEntity entity = personalDetailJpaRepository.save(PersonalDetailEntity.from(personalDetail));
+
+        return Optional.of(entity.toDomain());
+
+    }
 
     @Override
     public PersonalDetail saveIfNotExist(PersonalDetail personalDetail) {
@@ -31,8 +38,20 @@ public class PersonalDetailRepositoryImpl implements PersonalDetailRepository {
 
     @Override
     public PersonalDetail getById(Long personalDetailId) {
-        return personalDetailJpaRepository.findById(personalDetailId)
+        return personalDetailJpaRepository.findByIdJoinFetch(personalDetailId)
                 .orElseThrow(() -> new CustomException(ErrorCode.PERSONAL_DETAIL_NOT_FOUND))
                 .toDomain();
     }
+
+    @Override
+    public Optional<PersonalDetail> getTrainerDetail(Long trainerId) {
+        Optional<PersonalDetailEntity> findEntity = personalDetailJpaRepository.findByTrainerId(trainerId);
+        if (findEntity.isPresent()) {
+            return findEntity.map(PersonalDetailEntity::toDomain);
+        }
+
+        return Optional.empty();
+    }
+
+
 }
