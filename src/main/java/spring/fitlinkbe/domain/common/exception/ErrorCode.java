6d38ea1a91f543ef0bcb0827ce72fa0982e20479
hyunkyu <azdlgusrb@naver.com>
2025-02-07 22:08:24 +0900

    :bug:  "@Login 인증 에러 추가"
    Resolve : #FLIZ-54

--- src/main/java/spring/fitlinkbe/domain/common/exception/ErrorCode.java
+++ src/main/java/spring/fitlinkbe/domain/common/exception/ErrorCode.java
@@ -26,6 +26,7 @@ public enum ErrorCode {
     // Auth 관련 ErrorCode
     UNSUPPORTED_OAUTH_PROVIDER("지원하지 않는 OAuth 제공자입니다.", 400),
     EXPIRED_TOKEN("토큰이 만료되었습니다.", 401),
+    AUTH_FAILED("인증에 실패하였습니다.", 401),
     USER_STATUS_NOT_ALLOWED("해당 유저 상태에서는 요청 불가합니다.", 403),
     NEED_REQUIRED_SMS_STATUS("유저의 상태가 소셜 로그인만 진행된 상태어야 합니다", 403),
 
--- src/main/java/spring/fitlinkbe/support/argumentresolver/LoginMemberArgumentResolver.java
+++ src/main/java/spring/fitlinkbe/support/argumentresolver/LoginMemberArgumentResolver.java
@@ -8,6 +8,8 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import spring.fitlinkbe.domain.common.PersonalDetailRepository;
+import spring.fitlinkbe.domain.common.exception.CustomException;
+import spring.fitlinkbe.domain.common.exception.ErrorCode;
 import spring.fitlinkbe.support.security.AuthTokenProvider;
 import spring.fitlinkbe.support.security.SecurityUser;
 import spring.fitlinkbe.support.utils.HeaderUtils;
@@ -29,10 +31,13 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        String accessToken = HeaderUtils.getAccessToken(request);
-        Long personalDetailId = tokenProvider.getPersonalDetailIdFromAccessToken(accessToken);
-
-        return new SecurityUser(personalDetailRepository.getById(personalDetailId));
+        try {
+            HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+            String accessToken = HeaderUtils.getAccessToken(request);
+            Long personalDetailId = tokenProvider.getPersonalDetailIdFromAccessToken(accessToken);
+            return new SecurityUser(personalDetailRepository.getById(personalDetailId));
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.AUTH_FAILED);
+        }
     }
 }
