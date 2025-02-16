e8aa723d9ea772110cf51bc3fc55065dcc9f09b8
gt.park <smileboy0014@gmail.com>
2025-02-16 13:50:16 +0900

    :sparkles: :: "/v1/reservations/availability/disable", presentation Layer 개발 및 테스트 완료"

--- src/main/java/spring/fitlinkbe/interfaces/controller/reservation/ReservationController.java
+++ src/main/java/spring/fitlinkbe/interfaces/controller/reservation/ReservationController.java
@@ -1,13 +1,14 @@
 package spring.fitlinkbe.interfaces.controller.reservation;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import spring.fitlinkbe.application.reservation.ReservationFacade;
 import spring.fitlinkbe.interfaces.controller.common.dto.ApiResultResponse;
-import spring.fitlinkbe.interfaces.controller.reservation.dto.ReservationDetailDto;
-import spring.fitlinkbe.interfaces.controller.reservation.dto.ReservationDto;
+import spring.fitlinkbe.interfaces.controller.reservation.dto.ReservationRequestDto;
+import spring.fitlinkbe.interfaces.controller.reservation.dto.ReservationResponseDto;
 import spring.fitlinkbe.support.argumentresolver.Login;
 import spring.fitlinkbe.support.security.SecurityUser;
 
@@ -22,21 +23,51 @@ public class ReservationController {
 
     private final ReservationFacade reservationFacade;
 
+    /**
+     * 예약 목록 조회
+     *
+     * @param date 예약 정보를 받고 싶은 date 정보
+     * @param user 인증된 유저 정보
+     * @return ApiResultResponse 예약 목록을 반환한다.
+     */
     @GetMapping
-    public ApiResultResponse<List<ReservationDto.Response>> getReservations(@RequestParam LocalDate date,
-                                                                            @Login SecurityUser user) {
+    public ApiResultResponse<List<ReservationResponseDto.GetList>> getReservations(@RequestParam LocalDate date,
+                                                                                   @Login SecurityUser user) {
 
         return ApiResultResponse.ok(reservationFacade.getReservations(date, user).reservations().stream()
-                .map(ReservationDto.Response::of).toList());
+                .map(ReservationResponseDto.GetList::of).toList());
 
     }
 
-    @GetMapping("/{reservationId}")
-    public ApiResultResponse<ReservationDetailDto.Response> getReservation(@PathVariable("reservationId")
-                                                                           @NotNull Long reservationId) {
+    /**
+     * 예약 상세 조회
+     *
+     * @param reservationId reservationId
+     * @return ApiResultResponse 예약 상세 정보를 반환한다.
+     */
 
-        return ApiResultResponse.ok(ReservationDetailDto.Response.of(
+    @GetMapping("/{reservationId}")
+    public ApiResultResponse<ReservationResponseDto.GetDetail> getReservation(@PathVariable("reservationId")
+                                                                              @NotNull Long reservationId) {
+
+        return ApiResultResponse.ok(ReservationResponseDto.GetDetail.of(
                 reservationFacade.getReservation(reservationId)));
 
+    }
+
+    /**
+     * 예약 불가 설정
+     *
+     * @param request 예약 불가 설정할 date 정보
+     * @return ApiResultResponse 예약 불가 설정된 reservationId 정보를 반환한다.
+     */
+    @PostMapping("/availability/disable")
+    public ApiResultResponse<ReservationResponseDto.Success> setDisabledTime(@RequestBody @Valid
+                                                                             ReservationRequestDto.SetDisabledTime
+                                                                                     request,
+                                                                             @Login SecurityUser user) {
+
+        return ApiResultResponse.ok(ReservationResponseDto.Success.of(
+                reservationFacade.setDisabledReservation(request.toCriteria(user.getTrainerId()))));
     }
 }
