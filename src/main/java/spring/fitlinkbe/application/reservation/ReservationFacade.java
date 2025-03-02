dbe74b60105e5af27326d5ec2812203054b33a43
gt.park <smileboy0014@gmail.com>
2025-03-02 23:19:28 +0900

    :recycle: :: 예약 도메인의 컬럼 변경으로 리팩토링 (2/4)

--- src/main/java/spring/fitlinkbe/application/reservation/ReservationFacade.java
+++ src/main/java/spring/fitlinkbe/application/reservation/ReservationFacade.java
@@ -18,6 +18,7 @@ import spring.fitlinkbe.support.security.SecurityUser;
 import java.time.LocalDate;
 import java.util.List;
 
+import static spring.fitlinkbe.domain.common.enums.UserRole.MEMBER;
 import static spring.fitlinkbe.domain.common.enums.UserRole.TRAINER;
 import static spring.fitlinkbe.domain.notification.Notification.Reason;
 
@@ -42,14 +43,11 @@ public class ReservationFacade {
 
         //예약 상세 정보 조회
         Reservation reservation = reservationService.getReservation(reservationId);
-
         //세션 정보 조회
         Session session = reservationService.getSession(reservation.getStatus(),
                 reservationId);
-
         // 개인 정보 조회
         PersonalDetail personalDetail = memberService.getMemberDetail(reservation.getMember().getMemberId());
-
         // 조합해서 리턴
         return ReservationResult.ReservationDetail.from(reservation, session, personalDetail);
     }
@@ -57,11 +55,11 @@ public class ReservationFacade {
     @Transactional
     public Reservation setDisabledReservation(ReservationCriteria.SetDisabledTime criteria, SecurityUser user) {
         String cancelMessage = "예약 불가 설정";
-        //1.  현재 시간보다 뒤에 있는 모든 예약 조회
+        //1. 모든 예약 조회
         List<Reservation> reservations = reservationService.getReservations();
-        //2. 예약 불가능한 시간대에 예약이 있나 확인
-        List<Reservation> duplicatedReservations = reservations
-                .stream()
+        //2. 예약 불가능한 시간대나 현시간보다 뒤에 예약이 있나 확인
+        List<Reservation> duplicatedReservations = reservations.stream()
+                .filter(reservation -> reservation.isReservationAfterToday(reservation))
                 .map(reservation -> reservation.checkStatus() ? reservation : null)
                 .toList();
         //2-1. 만약 그러한 예약들이 있다면 모두 취소 시킴
@@ -80,30 +78,25 @@ public class ReservationFacade {
     }
 
     @Transactional
-    public ReservationResult.Reservations reserveSession(List<ReservationCriteria.ReserveSession> criteria
+    public Reservation reserveSession(ReservationCriteria.ReserveSession criteria
             , SecurityUser user) {
-        List<Reservation> reservations = criteria.stream()
-                .map(reserveSession -> reserveSession.
-                        toDomain(memberService.getSessionInfo(reserveSession.trainerId(),
-                                        reserveSession.memberId()),
-                                user))
-                .toList();
-        List<Reservation> savedReservation = reservationService.reserveSession(reservations);
-        //만약 트레이너가 예약을 했다면, 바로 세션 생성
-        reservationService.createSessions(savedReservation);
-        // 알람 전송
-        savedReservation.forEach(reservation -> {
-            if (user.getUserRole() == TRAINER) {
-                // 트레이너가 예약했다면 멤버에게 예약이 됐다는 알람 전송
-                PersonalDetail memberDetail = memberService.getMemberDetail(reservation.getMember().getMemberId());
-                notificationService.sendApproveReservationNotification(reservation.getReservationId(), memberDetail);
-            } else {
-                // 멤버가 예약했다면 트레이너에게 예약 요청을 했다는 알람 전송
-                PersonalDetail trainerDetail = trainerService.getTrainerDetail(reservation.getTrainer().getTrainerId());
-                notificationService.sendRequestReservationNotification(reservation, trainerDetail);
-            }
-        });
-        return ReservationResult.Reservations.from(savedReservation);
-    }
+        Reservation reservation = criteria.toDomain(memberService.getSessionInfo(criteria.trainerId(),
+                criteria.memberId()), user);
+        Reservation savedReservation = reservationService.reserveSession(reservation);
 
+        if (user.getUserRole() == TRAINER) {
+            //만약 트레이너가 예약을 했다면, 바로 세션 생성
+            reservationService.createSession(savedReservation);
+            // 트레이너가 예약했다면 멤버에게 예약이 됐다는 알람 전송
+            PersonalDetail memberDetail = memberService.getMemberDetail(reservation.getMember().getMemberId());
+            notificationService.sendApproveReservationNotification(savedReservation.getReservationId(), memberDetail);
+        }
+
+        if (user.getUserRole() == MEMBER) {
+            // 멤버가 예약했다면 트레이너에게 예약 요청을 했다는 알람 전송
+            PersonalDetail trainerDetail = trainerService.getTrainerDetail(reservation.getTrainer().getTrainerId());
+            notificationService.sendRequestReservationNotification(savedReservation, trainerDetail);
+        }
+        return savedReservation;
+    }
 }
