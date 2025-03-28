005df175acd93320684cd357cffe88b6927608ae
hyunkyu <azdlgusrb@naver.com>
2025-03-28 18:49:50 +0900

    :sparkles: "cors config 추가"
    Resolves : #FLIZ-165

--- src/main/java/spring/fitlinkbe/support/config/ApplicationYmlRead.java
+++ src/main/java/spring/fitlinkbe/support/config/ApplicationYmlRead.java
@@ -1,5 +1,6 @@
 package spring.fitlinkbe.support.config;
 
+import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +13,19 @@ import java.util.Map;
 public class ApplicationYmlRead {
     private Map<String, String> front;
 
+    @Getter
+    private CorsProperties cors;
+
     public String getFrontUrl() {
         return front.get("url");
     }
 
+    @Getter
+    @Setter
+    public static class CorsProperties {
+        private String allowedOrigins;
+        private String allowedMethods;
+        private String allowedHeaders;
+        private Long maxAge;
+    }
 }
