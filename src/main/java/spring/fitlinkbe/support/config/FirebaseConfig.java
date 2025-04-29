6969ff8f85182e7c2bf42edaead42d842fefbdd7
gt.park <smileboy0014@gmail.com>
2025-04-29 22:25:12 +0900

    :ambulance: firebase 리소스 경로 수정

--- src/main/java/spring/fitlinkbe/support/config/FirebaseConfig.java
+++ src/main/java/spring/fitlinkbe/support/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
@@ -11,10 +12,12 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${firebase.config-path}")
+    private String firebaseConfigPath;
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-service-account.json");
+        FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
