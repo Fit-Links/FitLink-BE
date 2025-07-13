1b4e995d165ac45d5f4db0b8724f2b0296816ee3
gt.park <smileboy0014@gmail.com>
2025-07-13 14:44:23 +0900

    :ambulance: 디버깅 위한 에러 객체 로그 찍기

--- src/main/java/spring/fitlinkbe/interfaces/controller/common/exception/ApiControllerAdvice.java
+++ src/main/java/spring/fitlinkbe/interfaces/controller/common/exception/ApiControllerAdvice.java
@@ -105,7 +105,7 @@ public class ApiControllerAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ApiResultResponse<Object> handlerException(Exception e) {
-        log.error("Exception is occurred! {}", e.getMessage());
+        log.error("Exception is occurred! {}", e);
         return ApiResultResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, false, e.getMessage(), null);
     }
 }
